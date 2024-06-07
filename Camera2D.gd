e4d64f5e0881ef8c90c4fa879724f45115d2c302
Questica <47101805+Questica@users.noreply.github.com>
2024-06-07 13:34:33 -0700

    built camera 2d functions
    camera 2d pans with arrow keys and by pushing edge of screen

--- Camera2D.gd
+++ Camera2D.gd
@@ -1,14 +1,17 @@
 extends Camera2D
 
-@onready var camera = $"."
+@onready var camera = self
+@onready var mouse_pos
+@onready var screen_size = get_viewport().size
 
-var viewportmousepos = get_global_mouse_position().x
-var mousepos = get_global_mouse_position().x
+@export var speed = 420.0
+@export var edge_pan_margin = 50
 
 func _process(delta):
-	pan(delta)
+	key_pan(delta)
+	mouse_pan(delta)
 
-func pan(delta):
+func key_pan(delta):
 	var pan_amount = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
 		pan_amount.x += 1
@@ -20,4 +23,21 @@ func pan(delta):
 		pan_amount.y -= 1
 	
 	pan_amount = pan_amount.normalized()
-	position += pan_amount * delta * 420
+	position += pan_amount * delta * speed
+
+func mouse_pan(delta):
+	mouse_pos = get_viewport().get_mouse_position()
+	var pan_amount = Vector2.ZERO
+
+	if mouse_pos.x >= screen_size.x - edge_pan_margin:
+		pan_amount.x += 1
+	elif mouse_pos.x <= edge_pan_margin:
+		pan_amount.x -= 1
+	
+	if mouse_pos.y >= screen_size.y - edge_pan_margin:
+		pan_amount.y += 1
+	elif mouse_pos.y <= edge_pan_margin:
+		pan_amount.y -= 1
+
+	pan_amount = pan_amount.normalized()
+	position += pan_amount * delta * speed
