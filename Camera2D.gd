dd168acc07dad37d5713ca7f458a122adda97d5e
Questica <47101805+Questica@users.noreply.github.com>
2024-06-09 14:58:05 -0700

    two huge fixes to camera

---
+++ Camera2D.gd
@@ -0,0 +1,67 @@
+extends Camera2D
+
+@onready var camera = self
+@onready var mouse_pos
+@onready var screen_size = get_viewport().size
+
+@export var speed = 420.0
+@export var edge_pan_margin = 50
+
+func _process(delta):
+	key_pan(delta)
+	mouse_pan(delta)
+
+func key_pan(delta):
+	var pan_amount = Vector2.ZERO
+	if Input.is_action_pressed("arrow_right"):
+		pan_amount.x += 1
+	if Input.is_action_pressed("arrow_left"):
+		pan_amount.x -= 1
+	if Input.is_action_pressed("arrow_down"):
+		pan_amount.y += 1
+	if Input.is_action_pressed("arrow_up"):
+		pan_amount.y -= 1
+	
+	pan_amount = pan_amount.normalized()
+	var new_position = position + pan_amount * delta * speed
+	
+	# Adjust limits by half the screen size
+	var adjusted_limit_left = limit_left + screen_size.x / 2
+	var adjusted_limit_right = limit_right - screen_size.x / 2
+	var adjusted_limit_top = limit_top + screen_size.y / 2
+	var adjusted_limit_bottom = limit_bottom - screen_size.y / 2
+	
+	# Clamp the new position to the adjusted camera limits
+	new_position.x = clamp(new_position.x, adjusted_limit_left, adjusted_limit_right)
+	new_position.y = clamp(new_position.y, adjusted_limit_top, adjusted_limit_bottom)
+	
+	position = new_position
+
+func mouse_pan(delta):
+	mouse_pos = get_viewport().get_mouse_position()
+	var pan_amount = Vector2.ZERO
+
+	if mouse_pos.x >= screen_size.x - edge_pan_margin:
+		pan_amount.x += 1
+	elif mouse_pos.x <= edge_pan_margin:
+		pan_amount.x -= 1
+	
+	if mouse_pos.y >= screen_size.y - edge_pan_margin + 50:
+		pan_amount.y += 1
+	elif mouse_pos.y <= edge_pan_margin:
+		pan_amount.y -= 1
+
+	pan_amount = pan_amount.normalized()
+	var new_position = position + pan_amount * delta * speed
+	
+	# Adjust limits by half the screen size
+	var adjusted_limit_left = limit_left + screen_size.x / 2
+	var adjusted_limit_right = limit_right - screen_size.x / 2
+	var adjusted_limit_top = limit_top + screen_size.y / 2
+	var adjusted_limit_bottom = limit_bottom - screen_size.y / 2
+	
+	# Clamp the new position to the adjusted camera limits
+	new_position.x = clamp(new_position.x, adjusted_limit_left, adjusted_limit_right)
+	new_position.y = clamp(new_position.y, adjusted_limit_top, adjusted_limit_bottom)
+	
+	position = new_position
